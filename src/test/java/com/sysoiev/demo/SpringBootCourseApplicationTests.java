032957f540db944a28f4dcd0dd6e327b23d2f2d9
Bada1208 <sysoievbogdan@gmail.com>
2020-12-29 13:21:55 +0100

    Initial commit

---
+++ src/test/java/com/sysoiev/demo/SpringBootCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysoiev.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
