28da5f71e89a9c75b7ad2f66e76a176577a51915
Bada1208 <sysoievbogdan@gmail.com>
2020-12-30 13:25:35 +0100

    add dao & service layers

---
+++ src/main/java/com/sysoiev/demo/service/StudentService.java
@@ -0,0 +1,36 @@
+package com.sysoiev.demo.service;
+
+import com.sysoiev.demo.dao.StudentDao;
+import com.sysoiev.demo.model.Student;
+
+import java.util.List;
+import java.util.UUID;
+
+public class StudentService {
+    private final StudentDao studentDao;
+
+    public StudentService(StudentDao studentDao) {
+        this.studentDao = studentDao;
+    }
+
+    public int persistNewStudent(UUID studentId, Student student) {
+        UUID studentUUID = studentId == null ? UUID.randomUUID() : studentId;
+        return studentDao.insertNewStudent(studentId, student);
+    }
+
+    public Student getStudentById(UUID studentId) {
+        return studentDao.selectStudentById(studentId);
+    }
+
+    public List<Student> getAllStudents() {
+        return studentDao.selectAllStudents();
+    }
+
+    public int updateStudentById(UUID studentId, Student studentUpdate) {
+        return studentDao.updateStudentById(studentId, studentUpdate);
+    }
+
+    public int deleteStudentById(UUID studentId) {
+        return studentDao.deleteStudentById(studentId);
+    }
+}
